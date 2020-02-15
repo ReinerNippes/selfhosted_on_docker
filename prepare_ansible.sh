190634953b9845ffcd6fbba2187e198f3a7e09c2
Reiner Nippes <reiner@nippes.it>
2020-02-15 12:01:04 +0000

    python3 debian/ubuntu

--- prepare_ansible.sh
+++ prepare_ansible.sh
@@ -1,96 +1,98 @@
 #!/bin/bash -uxe
 #
-# Prepare system for nextcloud devel
+# Prepare system for nextcloud installtion
 #
 
-install_pip () {
-	curl https://bootstrap.pypa.io/get-pip.py | $SUDO $PYTHON_BIN
-	$SUDO pip install setuptools -U
-	$SUDO pip install ansible -U
-	$SUDO pip install netaddr -U
-	$SUDO pip install dnspython -U
-	$SUDO pip install passlib -U
-	$SUDO pip install bcrypt -U
-	$SUDO pip install Jinja2 -U
+prepare_ubuntu() {
+        $SUDO apt update -y
+        $SUDO apt-get -o Dpkg::Options::="--force-confnew" -fuy dist-upgrade
+        $SUDO apt-get -o Dpkg::Options::="--force-confnew" -fuy install software-properties-common curl git mc vim facter python3 python3-setuptools python3-apt python3-pip python3-passlib python3-wheel python3-bcrypt aptitude -y
+        $SUDO [ $(uname -m) == "aarch64" ] && apt install gcc python3-dev libffi-dev libssl-dev make -y
+        $SUDO pip3 install ansible -U
+
+        set +x
+        echo
+        echo "   Ubuntu Sytem ready for nextcloud."
+        echo
+        ansible --version
 }
 
-prepare_ubuntu() { 
-	$SUDO apt update -y
-	$SUDO apt dist-upgrade -y
-	$SUDO apt install software-properties-common curl git mc vim facter python-minimal python-apt aptitude -y
-	$SUDO [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python-dev libffi-dev libssl-dev make -y
+prepare_debian() {
+        $SUDO apt update -y
+        $SUDO apt-get -o Dpkg::Options::="--force-confnew" -fuy dist-upgrade
+        $SUDO apt-get -o Dpkg::Options::="--force-confnew" -fuy install dirmngr curl git mc vim facter python3 python3-pip python3-apt python3-passlib python3-bcrypt python3-wheel python3-setuptools aptitude ansible -y
+        [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python3-dev libffi-dev libssl-dev make -y
+        $SUDO pip3 install ansible -U
 
-	PYTHON_BIN=/usr/bin/python
-	install_pip
-		
-	echo
-	echo "Ubuntu Sytem ready for nextcloud." 
-	echo
-}
-
-prepare_debian() { 
-	$SUDO apt update -y
-	$SUDO apt dist-upgrade -y
-	$SUDO apt install dirmngr curl git mc vim facter python python-apt -y
-	$SUDO [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python-dev libffi-dev libssl-dev make -y
-	
-	PYTHON_BIN=/usr/bin/python
-	install_pip
-	
-	echo
-	echo "Debian Sytem ready for nextcloud."
-	echo
+        set +x
+        echo
+        echo "   Debian Sytem ready for nextcloud."
+        echo
+        ansible --version
 }
 
 prepare_raspbian() {
-	$SUDO apt update -y
-	$SUDO apt dist-upgrade -y
-	$SUDO apt install dirmngr mc vim git libffi-dev curl facter gcc python-dev python-apt libffi-dev libssl-dev make -y
-	PYTHON_BIN=/usr/bin/python
-	install_pip
-	
-	echo
-	echo "Rasbpian System ready for nextcloud."
-	echo
+        $SUDO apt update -y
+        $SUDO apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+        $SUDO apt install dirmngr mc vim git libffi-dev curl facter python python-apt python-pip python-passlib python-bcrypt aptitude ansible -y
+        $SUDO pip install ansible -U
+
+        set +x
+        echo
+        echo "   Rasbpian System ready for nextcloud."
+        echo
+        ansible --version
 }
 
-prepare_centos() { 
-	$SUDO yum install epel-release -y
-	$SUDO yum install git vim mc curl facter libselinux-python -y
-	$SUDO yum update -y
-	PYTHON_BIN=/usr/bin/python
-	install_pip
-	
-	echo
-	echo "CentOS Sytem ready for nextcloud."
-	echo
+prepare_centos() {
+        $SUDO yum install epel-release -y
+        $SUDO yum install git vim mc curl facter libselinux-python python python-pip python-passlib python-bcrypt aptitude ansible -y
+        $SUDO yum update -y
+        $SUDO pip install ansible -U
+
+        set +x
+        echo
+        echo "   CentOS Sytem ready for nextcloud."
+        echo
+        ansible --version
 }
 
-prepare_amzn() { 
-	$SUDO amazon-linux-extras install epel
-	$SUDO yum install git vim mc curl facter libselinux-python -y
-	$SUDO yum update -y
-	PYTHON_BIN=/usr/bin/python
-	install_pip
-	
-	set +x
-	echo
-	echo "  Amazon Linux 2 ready for nextcloud."
-	echo
+prepare_fedora() {
+        $SUDO dnf install git vim mc curl facter python3 python3-dnf python3-pip python3-libselinux python3-bcrypt python3-passlib ansible -y
+        $SUDO dnf update -y
+
+        set +x
+        echo
+        echo "   Fedora Sytem ready for nextcloud."
+        echo
+        ansible --version
 }
 
-usage() { 
-	echo
-	echo "Linux distribution not detected."
-	echo "Use: ID=[Ubuntu|Debian|CentOS|raspbian] setup_ec2.sh"
-	echo "Other distributions not yet supported."
-	echo
+prepare_amzn() {
+        $SUDO amazon-linux-extras install epel -y
+        $SUDO yum install git vim mc curl facter libselinux-python python python-pip python-passlib python-bcrypt aptitude ansible -y
+        $SUDO yum update -y
+        $SUDO pip install ansible -U
+
+        set +x
+        echo
+        echo "   Amazon Linux 2 ready for nextcloud."
+        echo
+        ansible --version
 }
 
-if [  -f /etc/os-release ]; then 
-	. /etc/os-release
+usage() {
+        echo
+        echo "Linux distribution not detected."
+        echo "Use: ID=[ubuntu|debian|centos|raspbian|amzn|fedora] prepare_system.sh"
+        echo "Other distributions not yet supported."
+        echo
+}
+
+if [  -f /etc/os-release ]; then
+        . /etc/os-release
 elif [ -f /etc/debian_version ]; then
-	$ID=debian
+        $ID=debian
 fi
 
 # root or not
@@ -101,22 +103,27 @@ else
 fi
 
 case $ID in
-	'ubuntu')
-		prepare_ubuntu
-	;;
-	'debian')
-		prepare_debian
-	;;
-	'raspbian')
-		prepare_raspbian
-	;;
-	'centos')
-		prepare_centos
-	;;
-	'amzn')
-		prepare_amzn
-	;;
-	*)
-		usage
-	;;
+        'ubuntu')
+                prepare_ubuntu
+        ;;
+        'debian')
+                prepare_debian
+        ;;
+        'raspbian')
+                prepare_raspbian
+        ;;
+        'centos')
+                prepare_centos
+        ;;
+        'fedora')
+                prepare_fedora
+        ;;
+        'amzn')
+                prepare_amzn
+        ;;
+
+        *)
+                usage
+        ;;
 esac
+
